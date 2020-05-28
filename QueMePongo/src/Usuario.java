65aa847d08bd1e18c97b1f7ea7c520770f2e6f54
brianUtn98 <brian.gmonroy98@gmail.com>
2020-05-28 02:07:53 -0300

    Creado usuario

--- QueMePongo/src/Usuario.java
+++ QueMePongo/src/Usuario.java
@@ -2,11 +2,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Usuario {
+    Ciudad ciudad;
 List<Prenda> Guardarropas = new ArrayList<Prenda>();
 
 
     public void agregarPrenda(Prenda prenda){
         this.Guardarropas.add(prenda);
     }
+    public void generarPrenda(TipoDePrenda tipo,Color colorPrincipal,Color colorSecundario,Material material,Trama trama){
+        Borrador nuevaPrenda = new Borrador(tipo);
+        nuevaPrenda.especificarColorPrincipal(colorPrincipal);
+        nuevaPrenda.especificarColorSecundario(colorSecundario);
+        nuevaPrenda.especificarMaterial(material);
+        nuevaPrenda.especificarTrama(trama);
+        this.agregarPrenda(nuevaPrenda.crearPrenda());
+    }
+
 
 }
